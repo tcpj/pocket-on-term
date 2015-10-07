687d7a20698d8347ca686d4e2aea09e4ea378646
jaduse <jaduse@gmail.com>
2015-10-07 23:42:01 +0200

    version 0.3

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages
 
 
 
-setup(name="pocket-on-term",
-    version="0.1",
+setup(name="pocket-on-term_dev",
+    version="0.3",
     description="Pocket client for terminals",
     url="http://github.com/jaduse/pocket-on-term",
     author="Jakub Dusek",
@@ -15,7 +15,7 @@ setup(name="pocket-on-term",
     install_requires=["urwid"],
     entry_points={
             'console_scripts': [
-                'pot = pot:main',
+                'pot-dev = pot:main',
         ]
     }
 
